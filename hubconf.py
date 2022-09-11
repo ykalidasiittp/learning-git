6183ad969c2476f155bad6365b0dd9d10df784cf
ykalidas@iittp.ac.in <ykalidas@iittp.ac.in>
2022-09-11 18:57:16 +0530

    Example autograd for polynomial fitting

--- hubconf.py
+++ hubconf.py
@@ -3,16 +3,49 @@ dependencies = ['torch']
 import numpy as np
 import math
 
+dtype = torch.float
+device = torch.device("cpu")    
+
 def load_data():
 
     print ('loading data...')
     
-    x = np.linspace(-math.pi, math.pi, 2000)
-    y = np.sin(x)
+    x = torch.linspace(0, 1, 100, device=device, dtype=dtype)
+    y = 3 + 4 * x + 5 * x**2 + 6 * x**3
         
     return x, y
     
     
-def kali2():
-    print ('kali 2')
-    return
+def polyfit_autograd():
+
+    a = torch.randn((), device=device, dtype=dtype, requires_grad=True)
+    b = torch.randn((), device=device, dtype=dtype, requires_grad=True)
+    c = torch.randn((), device=device, dtype=dtype, requires_grad=True)
+    d = torch.randn((), device=device, dtype=dtype, requires_grad=True)
+
+    learning_rate = 1e-4
+
+    # 2022 Aug 23 - Kalidas (ykalidas@iittp.ac.in)
+    optimizer = torch.optim.SGD([a,b,c,d], lr=learning_rate, momentum=0)
+    
+    for t in range(10000):
+    
+        # Forward pass: compute predicted y using operations on Tensors.
+        y_pred = a + b * x + c * x ** 2 + d * x ** 3
+
+        # Compute and print loss using operations on Tensors.
+        # Now loss is a Tensor of shape (1,)
+        # loss.item() gets the scalar value held in the loss.
+        loss = (y_pred - y).pow(2).sum()
+
+        # 2022 Aug 23 - Kalidas (ykalidas@iittp.ac.in)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if t % 1000 == 99:
+            print(t, loss.item())    
+        
+    print(f'Result: y = {a.item()} + {b.item()} x + {c.item()} x^2 + {d.item()} x^3')
+        
+    return a, b, c, d
