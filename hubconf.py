b1329cd9edf686097040907154cdf89d17d49016
ykalidas@iittp.ac.in <ykalidas@iittp.ac.in>
2022-09-12 09:43:34 +0530

    Bug fix NN module

--- hubconf.py
+++ hubconf.py
@@ -125,7 +125,7 @@ def _test(dataloader, model, loss_fn):
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
     
-def train(train_dataloader, test_dataloader, epochs=5, model, loss_fn, optimizer):
+def train(train_dataloader, test_dataloader, model, loss_fn, optimizer, epochs=5):
     for t in range(epochs):
         print(f"Epoch {t+1}\n-------------------------------")
         train(train_dataloader, model, loss_fn, optimizer)
